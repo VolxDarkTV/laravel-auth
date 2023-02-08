756f04a30a033046c12ee2e337db5fe25b1c47eb
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-08 18:09:26 +0100

    portfolio in view

--- laravel-login-test/resources/views/pages/portfolio.blade.php
+++ laravel-login-test/resources/views/pages/portfolio.blade.php
@@ -1,13 +1,26 @@
 @extends('layouts.main-layouts')
 
 @section('main')
-    <h1>Portfolio</h1>
+<section class="container-fluid">
+    <div class="container d-flex flex-column align-items-center gap-4 mt-3">
 
-    <ul>
-        @foreach ($projects as $project)
-            <li>
-                {{ $project -> name }}
-            </li>
-        @endforeach
-    </ul>
+
+        <h1>Portfolio</h1>
+    
+        <ul class="d-flex justify-content-around flex-wrap gap-3">
+            @foreach ($projects as $project)
+                <li class="d-flex">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{$project -> project_img}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                          <p class="card-text">{{$project -> project_description}}</p>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+
+
+    </div>
+</section>
 @endsection
