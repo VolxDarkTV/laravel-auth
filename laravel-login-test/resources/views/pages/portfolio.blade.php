9f4cdefcb7e1c95c5912844dff29be45c4ef94dc
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-08 21:04:38 +0100

    add rainbow background

--- laravel-login-test/resources/views/pages/portfolio.blade.php
+++ laravel-login-test/resources/views/pages/portfolio.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.main-layouts')
 
 @section('main')
-<section class="container-fluid mt-5 pt-1">
-    <div class="container d-flex flex-column align-items-center gap-4 mt-5">
-
-
-        <h1>Portfolio</h1>
+<section id="portfolio">
+    <section class="my_background container-fluid mt-5 pt-1 pb-5">
+        <div class="container d-flex flex-column align-items-center gap-4 mt-5">
     
-        <ul class="d-flex justify-content-around flex-wrap gap-3">
-            @foreach ($projects as $project)
-                <li class="d-flex">
-                    <div class="card" style="width: 18rem;">
-                        <img src="{{$project -> project_img}}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <p class="card-text">{{$project -> project_description}}</p>
+    
+            <h1>Portfolio</h1>
+        
+            <ul class="d-flex justify-content-around flex-wrap gap-3">
+                @foreach ($projects as $project)
+                    <li class="d-flex">
+                        <div class="card" style="width: 18rem;">
+                            <img src="{{$project -> project_img}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">{{$project -> project_description}}</p>
+                            </div>
                         </div>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-
-
-    </div>
+                    </li>
+                @endforeach
+            </ul>
+    
+    
+        </div>
+    </section>
 </section>
 @endsection
