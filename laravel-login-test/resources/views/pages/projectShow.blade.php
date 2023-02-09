791095e954f3afb5b8f946aeaa73c53488bcfc75
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-09 13:49:03 +0100

    add show style

--- laravel-login-test/resources/views/pages/projectShow.blade.php
+++ laravel-login-test/resources/views/pages/projectShow.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.main-layouts')
 
 @section('main')
-<section class="container-fluid">
-    <section class="container mt-5 pt-5 text-dark d-flex justify-content-center">
+<section id="show">
+    <section class="my_background container-fluid">
 
-
-        <div class="card" style="width: 18rem;">
-            <img src="{{$project -> project_img}}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <span class="fw-bold">{{$project -> name}}</span>
-                <p class="card-text">{{$project -> project_description}}</p>
-            </div>
+        <div class="container mt-5 pt-5 text-dark d-flex justify-content-center">
+            <figure class="my_card image-block">
+                <h1>{{$project -> name}}</h1>
+                <img src="{{$project -> project_img}}" alt="" />
+                <figcaption class="rounded-top">
+    
+                    <h3>
+                        More Info
+                    </h3>
+    
+                    {{-- Description --}}
+                    <p>{{$project -> project_description}}</p>
+    
+                    {{-- Delete --}}
+                    @auth
+                        <div class="d-flex justify-content-center">
+                            <a class="btn btn-danger" href="{{route('project.delete', $project)}}">x</a>
+                        </div>
+                    @endauth
+    
+                </figcaption>
+            </figure>
         </div>
 
 
