d638aeaecd198315bea91523c379231433bc341c
VolxDarkTV <vinvenzo.foti@outlook.it>
2023-02-09 13:18:30 +0100

    add portfolio layout (card)

--- laravel-login-test/resources/views/pages/portfolio.blade.php
+++ laravel-login-test/resources/views/pages/portfolio.blade.php
@@ -11,7 +11,7 @@
             <ul class="d-flex justify-content-around flex-wrap gap-3">
                 @foreach ($projects as $project)
                     <li class="d-flex">
-                        <a href=" {{ route('project.show', $project) }} " class="card" style="width: 18rem;">
+                        {{-- <a href=" {{ route('project.show', $project) }} " class="card" style="width: 18rem;">
                             <img src="{{$project -> project_img}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <p class="card-text">{{$project -> project_description}}</p>
@@ -22,7 +22,32 @@
 
                             @endauth
                             
-                        </a>
+                        </a> --}}
+                        <figure class="my_card image-block">
+                            <h1>{{$project -> name}}</h1>
+                            <img src="{{$project -> project_img}}" alt="" />
+                            <figcaption>
+
+                                <h3>
+                                    More Info
+                                </h3>
+
+                                {{-- Description --}}
+                                <p>{{$project -> project_description}}</p>
+
+                                {{-- Show --}}
+                                <a class="btn btn-success" href="{{ route('project.show', $project) }}">
+                                    More Info
+                                </a>
+
+                                {{-- Delete --}}
+                                @auth
+                                    <a class="btn btn-danger" href="{{route('project.delete', $project)}}">x</a>
+                                @endauth
+
+                            </figcaption>
+                        </figure>
+                            
                     </li>
                 @endforeach
             </ul>
